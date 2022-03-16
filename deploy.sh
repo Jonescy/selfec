43bd2f403d328b51bd0c423f9c0f291b4e11127f
xjones <jonescyna@gmail.com>
2022-03-16 14:18:36 +0800

    fix deploy shell

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 echo "start deploying ...."
 git pull
+echo "cleaning build directory ...."
+rm -rf ./public/*
 echo  "building website ...."
 hugo -D
+echo "removing website directory"
+rm -rf /www/wwwroot/selfex.jonescy.com/*
 echo "Copy to the specified directory of the website ...."
 cp -r ./public/* /www/wwwroot/selfec.jonescy.com/
 echo "reload nginx configuration ...."
